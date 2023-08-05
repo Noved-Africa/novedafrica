7c00cd85ab2f8ee4d565e0492ad449739efafab5
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-08-05 23:48:26 +0100

    More

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+		<main className='flex min-h-screen flex-col items-center text-white justify-between p-24'>
+			HomePage
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quia consequuntur sint veniam odit officia perspiciatis sed, deserunt hic vitae, id porro saepe minima laboriosam odio, et corporis voluptatum? Iure.
+		</main>
 	);
 }
