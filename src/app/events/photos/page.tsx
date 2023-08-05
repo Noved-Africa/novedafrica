b0de833d430cccc51c594b6109c2712fc9a4797e
fullstack_daddy <aboyejiemmanuel07@gmail.com>
2023-08-05 13:36:20 +0100

    event-photos fix

--- src/app/events/photos/page.tsx
+++ src/app/events/photos/page.tsx
@@ -24,18 +24,33 @@ const Events_Photos = () => {
     <body className='text-white font-Albert_sans '>
 
       <Header/>
-      <span className='xs:collapse md:visible lg:ml-24 lg:absolute lg:-mt-[8rem] text-sm'>Media / Event Photos</span>
-      <div className='mx-5 md:mt-40'>
-        <p className='text-4xl text-center lg:ml-[28rem] md:ml-20 md:absolute md:-mt-[3rem] lg:mt-[7rem]'>Relive the moments, captured forever.</p>
-        <p className='text-center xs:mt-10 md:mx-36 md:mt-[1rem] md:mb-[1rem] lg:absolute lg:mt-[12rem] lg:ml-[30rem] '>Browse through our stunning event photos and immerse yourself <br/>in the memories that make our events truly unforgettable.</p>
-        <p className='w-3/12 xs:w-11/12 mx-auto lg:ml-[44rem] lg:absolute lg:mt-[16rem] '><Button text='View Photos'/></p>
+      <span className='xs:collapse md:visible md:absolute md:-mt-[14rem] md:ml-10 lg:ml-24 lg:absolute lg:-mt-[8rem] text-sm'>Media / Event Photos</span>
+      <div className='mx-5 md:mt-80'>
+        <p className='text-4xl text-center lg:ml-[28rem] md:ml-20 md:absolute md:-mt-[1.5rem] lg:mt-[7rem]'>Relive the moments, captured forever.</p>
+        <p className='text-center xs:mt-10 md:mx-36 md:absolute md:mt-[2rem] md:mb-[1rem] lg:absolute lg:mt-[12rem] lg:ml-[30rem] '>Browse through our stunning event photos and immerse yourself <br/>in the memories that make our events truly unforgettable.</p>
+        <p className='w-3/12 xs:w-11/12 mx-auto md:absolute md:mt-[8rem] lg:ml-[44rem] lg:absolute lg:mt-[16rem] '><Button text='View Photos'/></p>
       </div>
-      <Image src={picnic} alt='Our Photos Event Page' className='xs:-mt-[28rem] absolute lg:h-[44rem] -z-20 md:h-[40rem] w-screen xs:h-[30rem] lg:-mt-64'/>
+      <Image src={picnic} alt='Our Photos Event Page' className='xs:-mt-[28rem] absolute lg:h-[44rem] md:-mt-[20rem] -z-20 md:h-[40rem] w-screen xs:h-[30rem] lg:-mt-64'/>
       
       {/* For Mobile and Tablet Screens */}
 
-      <div className='lg:collapse xs:mt-[14rem] md:mt-40 lg:mt-[16rem] bg-[#FFFADE] grid xs:grid-rows-2 xs:mt-6 xs:mb-12'>
-        <form className='xs:mt-5 xs:grid xs:grid-cols-2 lg:grid-cols-none'>
+      {/* <div className='lg:collapse xs:mt-[4rem] md:mt-[13rem] w-screen bg-[#FFFADE] grid xs:grid-rows-2 xs:mb-12'>
+        <form className='xs:mt-5 xs:grid xs:grid-cols-2'>
+          <p><input type='text' placeholder='What are you looking for?' className='rounded-[3rem] border bg-white p-2 outline-none xs:pl-12 xs:text-sm xs:w-[16rem] md:w-[25rem] h-11 xs:ml-7'/>
+          <AiOutlineSearch className=' absolute xs:-mt-7 xs:ml-10' fill='grey' size={20}/></p>
+          <p className='xs:w-8/12 xs:-mt-3 md:mt-0 xs:ml-10'><Button text = 'Search'/></p>
+        </form>
+        <p className='grid xs:grid-cols-4 gap-1 h-5 mx-5'>
+          <Button text = 'All Photos'/>
+          <Button text = 'Events'/>
+          <Button text = 'Community'/>
+          <Button text = 'Hangout'/>
+        </p>
+        
+      </div> */}
+{/* Tablet Screen */}
+      <div className='lg:collapse md:mt-[13rem] w-screen bg-[#FFFADE] md:absolute md:mt-[20rem] grid xs:grid-rows-2 xs:mb-12'>
+        <form className='xs:mt-5 xs:grid xs:grid-cols-2'>
           <p><input type='text' placeholder='What are you looking for?' className='rounded-[3rem] border bg-white p-2 outline-none xs:pl-12 xs:text-sm xs:w-[16rem] md:w-[25rem] h-11 xs:ml-7'/>
           <AiOutlineSearch className=' absolute xs:-mt-7 xs:ml-10' fill='grey' size={20}/></p>
           <p className='xs:w-8/12 xs:-mt-3 md:mt-0 xs:ml-10'><Button text = 'Search'/></p>
@@ -51,7 +66,7 @@ const Events_Photos = () => {
 
       {/* For Desktop Screens */}
 
-      <div className='lg:visible xs:collapse absolute lg:mt-[8rem] bg-[#FFFADE] lg:w-screen pl-20 grid grid-cols-2'>
+      <div className='lg:visible xs:collapse lg:absolute lg:mt-[8rem] bg-[#FFFADE] lg:w-screen pl-20 grid grid-cols-2'>
         <p className='grid grid-cols-4 h-12 -mt-1'>
             <Button text = 'All Photos'/>
             <Button text = 'Events'/>
