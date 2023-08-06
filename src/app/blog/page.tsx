26cf46fffdb107614162481daff18f8fd164f113
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2023-08-06 20:28:10 +0100

    Blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,25 +9,25 @@ import BlogCard from "@/common/components/blogCard";
 
 const Blog = () => {
 	return (
-		<div className=' pt-32 relative '>
+		<div className=' lg:pt-32 pt-[90px] relative '>
 
 			<Image src={bgDesktop} alt="Background Image" className=" object-contain object-center hidden lg:block absolute top-0 -z-10" />
 			<Image src={bgTablet} alt="Background Image" className=" object-contain object-center hidden sm:block lg:hidden absolute top-0 -z-10" />
 			<Image src={bgMobile} alt="Background Image" className=" object-contain object-center block sm:hidden absolute top-0 -z-10" />
 
-			<p className=' text-black lg:pl-[100px] md:pl-8 pl-4 '>
+			<p className=' text-black lg:pl-[100px] md:pl-8 pl-4 md:text-[16px] text-sm '>
 				Media / Noved Blog
 			</p>
-			<div className=' h-full  lg:max-w-[839px] lg:px-0 md:px-[74px] px-4 mt-[100px]  mx-auto text-black text-center '>
-				<h1 className=' lg:text-[56px] text-4xl leading-[67px] font-bold md:mb-4 mb-3'>
+			<div className=' h-full  lg:max-w-[839px] lg:px-0 md:px-[74px] px-9 lg:mt-[100px] mt-10  mx-auto text-black text-center '>
+				<h1 className=' lg:text-[56px] text-4xl lg:leading-[67px] text-[#292929] leading-10 font-semibold md:mb-4 mb-4'>
 					Unleash your curiosity, explore our insightful blog.
 				</h1>
-				<p className=' text-[#424242] lg:text-lg text-[16px] tracking-tight '>
+				<p className=' text-[#424242] lg:text-lg text-[16px] lg:leading-7 leading-6 tracking-tight '>
 					Immerse yourself in a diverse range of captivating topics, expert
 					insights, and valuable resources on our blog, designed to enrich your
 					knowledge and spark meaningful conversations.
 				</p>
-				<button className='  bg-[#4A43CB] text-white px-6 py-[13px] mt-10 mb-20 rounded-full font-bold '>
+				<button className='  bg-[#4A43CB]  text-white px-6 py-[13px] mt-10 lg:mb-20 mb-[122px] rounded-full font-bold '>
 					Read our blog
 				</button>
 			</div>
@@ -36,11 +36,11 @@ const Blog = () => {
 				<SearchComponent text="Blog Spotlight: Explore our handpicked gems of inspiration." />
 			</div>
 
-			<div className=' bg-white flex px-[100px] pb-[100px] pt-[40px] gap-12  '>
+			<div className=' bg-white lg:flex-row flex-col  flex lg:px-[100px] px-4 pb-[100px] pt-[40px] gap-12  '>
 				<div className=' lg:w-3/5'>
 					<BlogImage image={blogImage} category={"Community"} />
 				</div>
-				<div className=' w-2/5 space-y-12 '>
+				<div className=' lg:w-2/5 space-y-12 '>
 					<div className=' border-l-2 border-[#4A43CB] pl-6 flex flex-col gap-2  '>
 						<h1 className=' text-[#292929] text-[24px] font-bold leading-9 '>
 							This is the blog spotlight heading 1 that would be at the top like
@@ -73,7 +73,7 @@ const Blog = () => {
 			<div>
 
 
-				<div className=" grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 px-[100px] gap-10 py-6 bg-white  ">
+				<div className=" grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:px-[100px] px-4 gap-10 py-6 bg-white  ">
 					<BlogCard />
 					<BlogCard />
 					<BlogCard />
